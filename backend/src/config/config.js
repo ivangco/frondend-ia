7e171c7c7014a38d7b49b721083773ed0a7fd50d
Clarus <clarus@example.com>
2025-06-06 00:41:18 -0300

    add assist

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     openai: {
         apiKey: process.env.OPENAI_API_KEY,
-        assistantId: process.env.OPENAI_ASSISTANT_ID || 'pruebas-cu'
+        assistantId: process.env.OPENAI_ASSISTANT_ID || 'asst_1CgpIHYX8ycfGWYvwIhOHpPq'
     },
     cors: {
         origin: process.env.CORS_ORIGIN || '*'
